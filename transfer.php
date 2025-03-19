c7b96894e726355c1ed12a3ba61a302e3d61e779
pushparajdev05 <pushparajdev05@gmail.com>
2025-03-19 17:54:24 +0530

    corrected the transaction table view

--- transfer.php
+++ transfer.php
@@ -188,6 +188,7 @@ include "./backend/db/defaulters/db_connection.php";
                 </tbody>
             </table>
         </div>
+    </div>
     <div id="defaulter_table" class="popUp">
         <div id="defaults_close" class="close_form">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
@@ -201,6 +202,7 @@ include "./backend/db/defaulters/db_connection.php";
                 
                 </tbody>
             </table>
+        </div>
         </div>
     <script>
             window.user="<?= $_SESSION["user"]?>";
